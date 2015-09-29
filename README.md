9ba33c4e0acc51b0da5bd03d6be93b85f0fd5033
Saad <saad.khan02@gmail.com>
2015-09-29 16:21:38 -0400

    Add script to know the world facing IP address.

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Collection of scripts that have helped me automate redundant things.
 
 ## Content
 
- * __repower__ - Tells whether or not an Ubuntu system needs to be rebooted.
+ * __repower.pl__ - Tells whether or not an Ubuntu system needs to be rebooted.
+ * __whatsmyip.pl__ - Returns the world facing IP address.
---
+++ whatsmyip.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+# This script gets you the external IP address whenever it gets changed.
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Expect;
+use Data::Dumper;
+
+use constant {
+    TIMEOUT_S => 10,
+    PROMPT => "\$ ",
+    IP_RECORD => "/tmp/ipRecord"
+};
+
+##
+# Gets the external IP address.
+#
+# @return external IP address for success.
+# @return undef for failure.
+#
+sub getIP()
+{
+    my $expect = new Expect;
+    $expect->log_stdout(0);
+
+    my $IP = undef;
+
+    $expect->spawn("/bin/bash");
+    $expect->expect(TIMEOUT_S, PROMPT);
+    $expect->send("lynx --dump http://ipecho.net/plain\n");
+    $expect->expect(TIMEOUT_S, PROMPT);
+    my $result = $expect->before();
+    $result =~ /([0-9]*[\.][0-9]*[\.][0-9]*[\.][0-9]*)/;
+    $IP = $1;
+
+    return $IP;
+}
+
+##
+# Checks whether the IP address has changed from the time we checked earlier.
+#
+# @param IP Current IP address of the machine.
+#
+# @return 1 IP has not changed.
+# @return 0 IP has changed.
+#
+sub checkEarlierIP($)
+{
+    my ($IP) = @_;
+
+    my $returnCode = 1;
+
+    if (-e IP_RECORD) {
+        newRecord($IP);
+    }
+}
+
+
+print Dumper getIP();
